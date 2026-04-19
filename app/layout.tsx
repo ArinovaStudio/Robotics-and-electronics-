e79da9f78b07fdb05233ef907d3b2a3b28468059
Arinova Studio <studio.arinova@gmail.com>
2026-04-19 16:43:46 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,8 +57,8 @@ export default function RootLayout({
       >
         <Providers>
           {children}
-          <Analytics />
         </Providers>
+        <Analytics />
 
                 {/* Google Analytics */}
         <Script
